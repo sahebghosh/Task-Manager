441a43a05aff6f80dc568d56981966c830c08e69
Saheb Ghosh <sahebghosh@Sahebs-MacBook-Air-2.local>
2025-04-22 12:39:47 +0200

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="text-center">
+      <h1 className="text-3xl font-bold text-blue-600">Task Manager App</h1>
+      <p className="mt-4 text-gray-700">Let’s build this app step by step 🚀</p>
+    </div>
+  );
+};
+
+export default Home;
